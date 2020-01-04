d753a855ea03481885b62e6b5a8ebb77eefb5f31
Youngsoo <pullinear@daum.net>
2020-01-04 15:17:12 +0900

    고사성어, 숙어, 단어 탐지하여 맨 위에 해석 추가 출력 결과 보기 좋게 변경

--- main.py
+++ main.py
@@ -1,34 +1,55 @@
 from search import NCSearch
 
 already_searched = {}
-result_word = ""
 
+result_file_name = "result"
 if __name__ == '__main__':
     NCS = NCSearch()
+    with open(result_file_name + ".txt", "w", encoding="utf-8") as wf:
+        wf.write("result\n")
 
     with open("read.txt", "r", encoding="utf-8") as f:
         for line in f.readlines():
-            for c in line.strip():
-                if ord('가') <= ord(c) <= ord('힣'):
-                    pass
-                elif c in "\n\t\r ":
+            # 여백으로 라인을 단어로 쪼갬
+            for word in line.strip().split(" "):
+                with open(result_file_name + ".txt", "a", encoding="utf-8") as wf:
+                    wf.write("-------------------------------------------------\n")
+                clean_word = ""
+                for c in word:
+                    if ord('가') <= ord(c) <= ord('힣'):
+                        pass
+                    elif c in "\n\t\r ":
+                        pass
+                    else :
+                        clean_word += c
+
+                # 한자로만 이루어진 단어로 검색
+                sound, meaning = NCS.searchLetters(clean_word)
+                if sound is None or meaning is None:
                     pass
                 else:
-                    result_word += c
+                    with open(result_file_name+".txt", "a", encoding="utf-8") as wf:
+                        print(clean_word + ' : ' + sound + '\n   ' + meaning, end="\n")
+                        wf.write(clean_word + ' : ' + sound + '\n   ' + meaning + "\n")
+                        wf.close()
+                for c in clean_word:
                     if c in already_searched.keys():
                         sound = already_searched[c][0]
-                        result = already_searched[c][1]
+                        meaning = already_searched[c][1]
                     else:
                         sound, meaning = NCS.search(c)
-                        already_searched[c] = (sound, meaning)
-                    print(c + ' : ' + sound + '\n' + meaning, end="\n")
-    print("검색 완료")
-    with open("result.txt", "w", encoding="utf-8") as wf:
-        for c in result_word:
-            if c in already_searched.keys():
-                wf.write(c + ' : ' + already_searched[c][0] + '\n' + already_searched[c][1] + "\n")
+                        if sound is None or meaning is None:
+                            continue
+                        else:
+                            already_searched[c] = (sound, meaning)
+                    print(c + ' : ' + sound + '\n   ' + meaning, end="\n")
+                    with open(result_file_name + ".txt", "a", encoding="utf-8") as wf:
+                        wf.write(c + ' : ' + sound + '\n   ' + meaning + "\n")
+                        wf.close()
+                with open(result_file_name + ".txt", "a", encoding="utf-8") as wf:
+                    wf.write("-------------------------------------------------\n\n")
 
-        print("쓰기 완료")
+    print("완료")
 
     NCS.close()
 
--- search.py
+++ search.py
@@ -1,20 +1,57 @@
+import selenium
 from selenium import webdriver
 import time
 import urllib.parse
+
+from selenium.common.exceptions import NoSuchElementException
 
 
 class NCSearch:
     def __init__(self):
         self.driver = webdriver.Chrome("./driver/chromedriver.exe")
     # Get the chinese character and Return meaning of the character
-    def search(self, c: str):
+    def search(self, c:str):
         query = urllib.parse.quote(c, safe='')
         self.driver.get("https://hanja.dict.naver.com/search?query=" + query)
         # this condition can be changed
         time.sleep(2)
-        sound = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd/a[1]/span""")
-        meaning = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd/div""")
+        try:
+            sound = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd/a[1]/span""")
+            meaning = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd/div""")
+        except NoSuchElementException:
+            return None, None
+
         return sound.text, meaning.text
+
+    def searchLetters(self, s: str):
+        q_string = ""
+        for c in s:
+            query = urllib.parse.quote(c, safe='')
+            q_string += query
+
+        self.driver.get("https://hanja.dict.naver.com/search?query=" + q_string)
+        # this condition can be changed
+        time.sleep(2)
+
+        try:
+            check_word = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/ul/li[2]/a""").text
+            if check_word == "고사성어":
+                pass
+            else:
+                return None, None
+            sound = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd[1]/a[1]/span""")
+            meaning = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd[2]""")
+            return sound.text, meaning.text
+        except NoSuchElementException:
+            try:
+                check_word = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/h5/span[1]/span""")
+                sound = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd[1]/a[1]/span""")
+                meaning = self.driver.find_element_by_xpath("""//*[@id="content"]/div[3]/dl/dd[2]""")
+                return sound.text, meaning.text
+
+            except NoSuchElementException:
+                print("고사성어 숙어 단어 없음")
+                return None, None
 
     def close(self):
         self.driver.close()
